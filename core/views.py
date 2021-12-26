2f07e27173180239645380f28dca5825a3c88a65
tranhien <tranthuhien_t64@hus.edu.vn>
2021-12-26 22:20:30 +0700

    custom cart and add contact view

--- core/views.py
+++ core/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.views import View
+from django.views.generic.base import TemplateView
 # Create your views here.
 
 class HomeView(View):
     def get(self, request):
         return render(request, 'homepage/index.html')
+
+class ContactClass(TemplateView):
+    template_name="core/contacts.html"
